b8b221938d9d9ae43689e1a855979b4208ac8a32
gedjerone <antittter@gmail.com>
2022-06-26 19:01:14 +0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,28 +1,29 @@
 class Cart {
     render() {
-        localStorageUtil.forEach(element => {
+        let html_cart = '';
+
+        localStorageUtil.getProductsAllContent().forEach(element => {
             html_cart += `
                 <tr>
-                    <td scope="row" data-label="Вес/штука">${weight.toFixed(3)}</td>
-                    <td data-label="№ по каталогу">${catalog_id}</td>
-                    <td data-label="Наименование">${title}</td>
-                    <td data-label="Цена с НДС, руб/штука">${price.toLocaleString()}</td>
-                    <td data-label="Количество">${count}</td>
-                    <td data-label="Вес/итого">${weight_sum.toFixed(3)}</td>
-                    <td data-label="Цена с НДС, руб/итого">${price_sum.toLocaleString()}</td>
+                    <td scope="row" data-label="Вес/штука">${element[3].toFixed(3)}</td>
+                    <td data-label="№ по каталогу">${element[6]}</td>
+                    <td data-label="Наименование">${element[7]}</td>
+                    <td data-label="Цена с НДС, руб/штука">${element[2].toLocaleString()}</td>
+                    <td data-label="Количество">${element[1]}</td>
+                    <td data-label="Вес/итого">${element[5].toFixed(3)}</td>
+                    <td data-label="Цена с НДС, руб/итого">${element[4].toLocaleString()}</td>
                     <td data-label=" ">
                         <div class="counter">
                             <div class="text-field__group">
-                                <div class="hidden_id">${element}</div>
-                                <button class="text-field__btn_2_hide" type="button">Удалить из корзины</button>
+                                <div class="hidden_id">${element[0]}</div>
+                                <button class="cart_remove__btn type="button">Удалить из корзины</button>
                             </div>
                         </div>
                     </td>  
                 </tr>
             `
         });
-
-        const html = `
+        const cart_html = `
         <table class="cart_content tb-1">
             <thead>
                 <tr>
@@ -41,10 +42,12 @@ class Cart {
             </tbody>
         </table>
         `;
-
-        document.getElementById('cart_content').innerHTML = html;
+        document.getElementById('cart_content').innerHTML = cart_html;
+        var newScript = document.createElement("script");
+        newScript.src = "js/remove_from_cart.js";
+        document.getElementById('cart_content').appendChild(newScript)
     }
 }
 
-const cart  = new Cart();
+const cart = new Cart();
 cart.render();
