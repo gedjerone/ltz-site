864165c0dc255016f05e9ea061e8230290cde544
gedjerone <antittter@gmail.com>
2022-07-02 09:40:32 +0300

    Create Other.js

---
+++ js/data/Other.js
@@ -0,0 +1,105 @@
+var Other = [{
+        "id": 54,
+        "category": "Прочее",
+        "weight": 0,
+        "catalog_id": "—",
+        "title": "р/к Р-80-3/1-222",
+        "price": 75
+    },
+    {
+        "id": 55,
+        "category": "Прочее",
+        "weight": 0,
+        "catalog_id": "—",
+        "title": "р/к ПВМ",
+        "price": 374
+    },
+    {
+        "id": 56,
+        "category": "Прочее",
+        "weight": 0,
+        "catalog_id": "—",
+        "title": "р/к НШ-32У",
+        "price": 45
+    },
+    {
+        "id": 57,
+        "category": "Прочее",
+        "weight": 0,
+        "catalog_id": "—",
+        "title": "р/к напрпвл.втулок штанг клапанов (19шт).",
+        "price": 160
+    },
+    {
+        "id": 58,
+        "category": "Прочее",
+        "weight": 0,
+        "catalog_id": "—",
+        "title": "шина 13,6-38 без камеры",
+        "price": 7600
+    },
+    {
+        "id": 59,
+        "category": "Прочее",
+        "weight": 0.05,
+        "catalog_id": "—",
+        "title": "ремкомплект ГУР с прокладкой",
+        "price": 110
+    },
+    {
+        "id": 60,
+        "category": "Прочее",
+        "weight": 0,
+        "catalog_id": "—",
+        "title": "р/к ЦС-90",
+        "price": 60
+    },
+    {
+        "id": 61,
+        "category": "Прочее",
+        "weight": 0,
+        "catalog_id": "—",
+        "title": "муфта соединительная (штуцер) ключ 24ГР.S24**",
+        "price": 38
+    },
+    {
+        "id": 62,
+        "category": "Прочее",
+        "weight": 1.096,
+        "catalog_id": "Т40А-2300001-А",
+        "title": "комплект стопорных шайб (ремкомплект ПВМ)",
+        "price": 265
+    },
+    {
+        "id": 63,
+        "category": "Прочее",
+        "weight": 2.13,
+        "catalog_id": "Т40М1-6700014",
+        "title": "уплотнитель стекла ЛТЗ (2,4м)",
+        "price": 1100
+    },
+    {
+        "id": 64,
+        "category": "Прочее",
+        "weight": 0,
+        "catalog_id": "Т40М1-6700014*",
+        "title": "уплотнитель стекла ЛТЗ",
+        "price": 280
+    },
+    {
+        "id": 65,
+        "category": "Прочее",
+        "weight": 0,
+        "catalog_id": "Т25-6703027",
+        "title": "уплотнитель Т-40 НТ8",
+        "price": 38
+    },
+    {
+        "id": 66,
+        "category": "Прочее",
+        "weight": 0,
+        "catalog_id": "Т25-6709012-А",
+        "title": "уплотнитель стекла Т-40 НТ9",
+        "price": 120
+    }
+]
