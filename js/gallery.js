21ce8a273142b4795923b719aa03208ff2123078
gedjerone <antittter@gmail.com>
2022-07-20 08:28:02 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -2,6 +2,8 @@ var photos = document.querySelectorAll('.photo');
 var main = document.querySelector('.main');
 var content = document.querySelector('.content');
 var body = document.querySelector('body');
+var left_arrow = document.querySelector('.left_arrow');
+var right_arrow = document.querySelector('.right_arrow');
 
 function changePhoto(element) {
     main.src = element.src;
@@ -9,6 +11,26 @@ function changePhoto(element) {
 
 function magnify(element) {
     document.querySelector('.linker').href = element.src;
+}
+
+function changePhotoMobile() {
+    var arr = [];
+    for (let k = 0; k < photos.length; k++) {
+        arr[k] = photos[k].src;
+    }
+    for (let i = 0; i < photos.length; i++) {
+        if (main.src == photos[i].src) {
+            let temp = arr[i];
+            arr.splice(i, 1);
+            arr.push(temp);
+        }
+    }
+    var arr_copy = Object.assign([], arr);
+    for (let j = 0; j < photos.length; j++) {
+        let temp = arr_copy[j];
+        photos[j].src = temp;
+    }
+    main.src = photos[0].src;
 }
 
 photos.forEach(element => {
@@ -22,5 +44,19 @@ photos.forEach(element => {
 main.addEventListener('click', function(event) {
     if (event.target.closest('.main')) {
         magnify(main);
+    }
+});
+
+left_arrow.addEventListener('click', function(event) {
+    if (event.target.closest('.left_arrow')) {
+        console.log("левая");
+        changePhotoMobile();
+    }
+});
+
+right_arrow.addEventListener('click', function(event) {
+    if (event.target.closest('.right_arrow')) {
+        console.log("правая");
+        changePhotoMobile();
     }
 });
